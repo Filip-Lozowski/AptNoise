56fcdeada326053f54932719239bcf3eaa2d5f0d
Filip Łozowski <filiplozowski@gmail.com>
2022-01-23 14:36:41 +0100

    more functional tests

--- app.py
+++ app.py
@@ -1,1 +1,1 @@
-from dataprep import download_articles, transform_raw_data, save_labeled_data
+
---
+++ tests/functional_tests.py
@@ -0,0 +1,23 @@
+# open the app
+
+# download the articles / be able to look at them
+
+# The most interesting articles (those containing higher scores) are closer to the top
+
+# The least interesting articles are at the bottom
+
+# Click at the first article title to read it on its original page
+
+# assign a score to the article
+
+# assign scores to half of the articles
+
+# Save all scores
+
+# Retrain the model
+
+# Reload the app
+
+# Previously rated articles disappeared
+
+# Not sure -> The order of remaining articles changed (as there is some randomness involved)
