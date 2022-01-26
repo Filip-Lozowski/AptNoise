7f608b28306c75a2db9ea36356bddf960a866223
Filip Łozowski <filiplozowski@gmail.com>
2022-01-26 17:04:40 +0100

    renaming functional_tests.py so that pytest can find it + a class for func tests with setup and teardown

---
+++ tests/test_functional_tests.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+
+
+class TestUsersPerspective:
+    @classmethod
+    def setup_class(cls):
+        cls.browser = webdriver.Firefox()
+
+    @classmethod
+    def teardown_class(cls):
+        cls.browser.quit()
+
+    def test_read_rate_and_reload(self):
+        # open the app
+        self.browser.get('http://localhost:5000')
+
+        # check if this is a news app
+        assert "AptNoise" in self.browser.title
+
+        # click a button to download articles
+
+        # The most interesting articles (those containing higher scores) are closer to the top
+
+        # The least interesting articles are at the bottom
+
+        # Click at the first article title to read it on its original page
+
+        # assign a score to the article by typing it into a box near the link
+
+        # assign scores to half of the articles
+
+        # Save all scores
+
+        # Retrain the model
+
+        # Reload the app
+
+        # Previously rated articles disappeared
+
+        # Not sure -> The order of remaining articles changed (as there is some randomness involved)
