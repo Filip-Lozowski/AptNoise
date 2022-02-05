c37c38e11fe09bfae4f80afee7785bfa59b80012
Filip Łozowski <filiplozowski@gmail.com>
2022-02-05 10:13:13 +0100

    testing the home page template

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,8 +3,24 @@ import os
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-from flask import url_for, request
+from flask import url_for, request, template_rendered
 from app import app
+import pytest
+
+
+@pytest.fixture
+def captured_templates(app):
+    recorded = []
+
+    def record(sender, template, context, **kwargs):
+        recorded.append((template, context))
+
+    template_rendered.connect(record, app)
+
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
 
 
 class TestHomePage:
@@ -14,9 +30,15 @@ class TestHomePage:
             self.url = url_for('index')
         assert self.url == '/'
 
-    def test_home_page_returns_correct_html(self):
+    def test_home_page_returns_correct_html(self, captured_templates):
         with app.test_client() as test_client:
             response = test_client.get('/')
             assert response.data.startswith(b'<!DOCTYPE html>')
             assert b'<title>AptNoise</title>' in response.data
             assert response.data.endswith(b'</html>\n')
+
+            assert len(captured_templates) == 1
+
+            template, context = captured_templates[0]
+
+            assert template.name == "something.html"
