39e506b5b3fc79570f1414582c76c15ad4551eec
Filip Łozowski <filiplozowski@gmail.com>
2022-02-05 19:58:59 +0100

    functional test passed

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from dataprep import download_articles
+from config import api_key
+
+api_url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='
+response = download_articles(api_url, api_key)
+articles_list = response.json()['articles']
+links_list = []
+for article in articles_list:
+    links_list.append(article['url'])
 
 
 def create_app():
@@ -8,6 +17,6 @@ def create_app():
 
     @app.route('/')
     def index():
-        return render_template('index.html')
+        return render_template('index.html', links_list=links_list)
 
     return app
--- tests/test_dataprep.py
+++ tests/test_dataprep.py
@@ -13,7 +13,7 @@ def test_download_articles_response_ok():
 
     assert response.status_code == 200
 
-
+# można to zamienić na jeden test z dwoma assertami zamiast te dwa oddzielnie
 def test_download_articles_is_json():
     api_url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey='
     response = dp.download_articles(api_url, api_key)
