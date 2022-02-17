b7794ca26576c5cb039747e421f79783a2e97210
Filip <filiplozowski@gmail.com>
2022-02-17 19:03:50 +0100

    refactor app.py

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ from config import api_key
 api_url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='
 response = download_articles(api_url, api_key)
 articles_list = response.json()['articles']
-links_list = [] # refactor na list comprehension
-for article in articles_list:
-    links_list.append(article['url'])
 
 db = SQLAlchemy()
 
