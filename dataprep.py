50e623297217e45abe4a6bc025006f186dd30b1b
Filip <filiplozowski@gmail.com>
2022-02-17 10:51:08 +0100

    deleted forward-thinking functions

--- dataprep.py
+++ dataprep.py
@@ -6,19 +6,3 @@ def download_articles(api_url, key):
     response = requests.get(articles_url)
 
     return response
-
-
-def transform_response_into_df():
-    return None
-
-
-def transform_raw_data():
-    return None
-
-
-def load_labeled_data():
-    return None
-
-
-def save_labeled_data():
-    return None
