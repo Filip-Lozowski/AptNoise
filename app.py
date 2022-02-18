62f504b674ef1149c010a525bc84100a59b793bc
Filip <filiplozowski@gmail.com>
2022-02-18 10:32:30 +0100

    refactor app to use pandas df

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from dataprep import download_articles
 from config import api_key
+import pandas as pd
 
 api_url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='
 response = download_articles(api_url, api_key)
-articles_list = response.json()['articles']
+# articles_list = response.json()['articles']
+articles_df = pd.json_normalize(response.json()['articles'])
 
 db = SQLAlchemy()
 
@@ -33,13 +35,13 @@ def create_app():
     @app.route('/', methods=['GET', 'POST'])
     def index():
         if request.method == 'GET':
-            return render_template('index.html', link=articles_list[0]['url'])
+            return render_template('index.html', link=articles_df.loc[articles_df.index[0], 'url'])
         if request.method == 'POST':
             score = request.form['item_score']
-            rated_article = Record(source=articles_list[0]['source']['name'], score=score)
+            rated_article = Record(source=articles_df.loc[articles_df.index[0], 'source.name'], score=score)
             db.session.add(rated_article)
             db.session.commit()
-            del articles_list[0]
+            articles_df.drop(index=articles_df.index[0], inplace=True)
             return redirect(url_for("index"))
 
     @app.shell_context_processor
