67b559eb5c9e340eca7350fac3cbeec896add5c5
Filip <filiplozowski@gmail.com>
2022-02-18 11:20:59 +0100

    exchange label encoder for ordinal encoder

--- model_training.py
+++ model_training.py
@@ -1,7 +1,7 @@
 import pickle
 import sqlite3
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OrdinalEncoder
 from sklearn.tree import DecisionTreeRegressor
 
 con = sqlite3.connect('site.db')
@@ -13,11 +13,11 @@ articles = cur.fetchall()
 articles_df = pd.DataFrame(articles, columns=['id', 'source_name', 'score'])
 articles_df.set_index('id', inplace=True)
 
-encoder = LabelEncoder()
-encoder.fit(articles_df['source_name'])
+encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=999)
+encoder.fit(articles_df['source_name'].values.reshape(-1, 1))
 
 training_data = articles_df.copy()
-training_data['source_name'] = encoder.transform(training_data['source_name'])
+training_data['source_name'] = encoder.transform(training_data['source_name'].values.reshape(-1, 1))
 x_train = training_data.drop(columns=['score'])
 y_train = training_data['score']
 
