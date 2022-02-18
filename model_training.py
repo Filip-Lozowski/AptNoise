c373684a6ebb2d6e4b95cf688824605ec4667c08
Filip <filiplozowski@gmail.com>
2022-02-18 09:46:50 +0100

    ML model training and saving

--- model_training.py
+++ model_training.py
@@ -1,5 +1,8 @@
+import pickle
 import sqlite3
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeRegressor
 
 con = sqlite3.connect('site.db')
 cur = con.cursor()
@@ -9,4 +12,17 @@ articles = cur.fetchall()
 
 articles_df = pd.DataFrame(articles, columns=['id', 'source_name', 'score'])
 articles_df.set_index('id', inplace=True)
-print(articles_df.head())
+
+encoder = LabelEncoder()
+encoder.fit(articles_df['source_name'])
+
+training_data = articles_df.copy()
+training_data['source_name'] = encoder.transform(training_data['source_name'])
+x_train = training_data.drop(columns=['score'])
+y_train = training_data['score']
+
+model = DecisionTreeRegressor()
+model.fit(x_train, y_train)
+
+pickle.dump(encoder, open('source_encoder.pkl', 'wb'))
+pickle.dump(model, open('ml_model.pkl', 'wb'))
