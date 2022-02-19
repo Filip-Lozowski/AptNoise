2aa5732ca5e47866e11f509f3638e08caf5448ae
Filip <filiplozowski@gmail.com>
2022-02-19 09:31:18 +0100

    fixed unit tests wiping out the db

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,6 +11,12 @@ import pytest
 @pytest.fixture()
 def app():
     app = create_app()
+    app.config.update(
+        {
+            'TESTING': True,
+            'SQLALCHEMY_DATABASE_URI': 'sqlite:///test_site.db'
+         }
+    )
     with app.app_context():
         db.create_all()
         yield app
