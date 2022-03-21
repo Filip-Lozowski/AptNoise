59e366e8432e26deb7296c06ec74105692b2111a
Filip <filiplozowski@gmail.com>
2022-03-21 17:01:33 +0100

    small refactor: import functions instead of the whole dataprep module

--- tests/test_dataprep.py
+++ tests/test_dataprep.py
@@ -4,18 +4,18 @@ import os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 from config import api_key
-import dataprep as dp
+from dataprep import download_articles
 
 
 def test_download_articles_response_ok():
     api_url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='
-    response = dp.download_articles(api_url, api_key)
+    response = download_articles(api_url, api_key)
 
     assert response.status_code == 200
 
 
 def test_download_articles_is_json():
     api_url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey='
-    response = dp.download_articles(api_url, api_key)
+    response = download_articles(api_url, api_key)
 
     assert isinstance(response.json(), dict)
