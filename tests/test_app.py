3ff7df25059428dba4e9cbcfbd41e9685c614ad0
Filip <filiplozowski@gmail.com>
2022-03-22 17:26:30 +0100

    modify the test for ml model

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 from flask import url_for, request, template_rendered
 from app import create_app, db, Record
-from dataprep import download_articles, prepare_articles
+from dataprep import download_articles, prepare_articles, derive_content_length
 from config import api_key
 import pytest
 import pickle
@@ -115,6 +115,9 @@ class TestMLModel:
         cat_cols = ['author', 'source_name']
         encoder = pickle.load(open('source_encoder.pkl', 'rb'))
         input_data[cat_cols] = encoder.transform(input_data[cat_cols])
+
+        input_data['content_length_chars'] = input_data.apply(derive_content_length, axis=1)
+        input_data.drop(columns='content', inplace=True)
 
         predictions = ml_model.predict(input_data)
         assert predictions.any()
