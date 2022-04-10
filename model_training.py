99cb35eab02d3477101c7ce55ee9d19e06629516
Filip <filiplozowski@gmail.com>
2022-04-10 16:14:02 +0200

    Use a new function to prepare data

--- model_training.py
+++ model_training.py
@@ -1,18 +1,11 @@
 import pickle
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.tree import DecisionTreeRegressor
-from dataprep import db_to_df, derive_content_length
+from dataprep import db_to_df, derive_content_length, db_into_ml
 
 
 def model_training(test):
-    articles = db_to_df()
-    articles = articles[articles['assigned_score'] != -999]
-    articles = articles.drop_duplicates(subset=['title', 'published_at'])
-    articles.dropna(subset='content', inplace=True)
-    articles['content_length_chars'] = articles.apply(derive_content_length, axis=1)
-    articles.dropna(subset='content_length_chars', inplace=True)
-
-    training = articles[articles['is_test_record'] == 0]
+    training = db_into_ml(set_type='training')
     y_train = training['assigned_score']
 
     feature_cols = ['author', 'source_name', 'content_length_chars']
