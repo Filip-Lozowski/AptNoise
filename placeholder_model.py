72a298ecd387c5417e50c69eadc2a07032db6309
Filip <filiplozowski@gmail.com>
2022-04-18 12:24:14 +0200

    Add testing and generation of a placeholder encoder

--- placeholder_model.py
+++ placeholder_model.py
@@ -2,6 +2,11 @@ import pickle
 import numpy as np
 
 
+class PlaceholderEncoder:
+    def transform(self, input_data):
+        return input_data
+
+
 class PlaceholderModel:
     def predict(self, input_data):
         n_predictions = len(input_data)
@@ -14,5 +19,8 @@ def generate_placeholder_model(test):
     else:
         suffix = ''
 
+    encoder = PlaceholderEncoder()
     model = PlaceholderModel()
+
+    pickle.dump(encoder, open(f'cat_cols_encoder{suffix}.pkl', 'wb'))
     pickle.dump(model, open(f'ml_model{suffix}.pkl', 'wb'))
--- tests/test_placeholder_model.py
+++ tests/test_placeholder_model.py
@@ -6,7 +6,19 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import pickle
 import pandas as pd
 from placeholder_model import generate_placeholder_model
-from dataprep import new_data_into_ml_features
+from dataprep import get_new_articles_df, prepare_new_articles, create_features, CAT_COLS, new_data_into_ml_features
+
+
+def test_placeholder_encoder_works_on_real_data():
+    generate_placeholder_model(test=True)
+    articles_df = get_new_articles_df()
+    articles_df = prepare_new_articles(articles_df)
+    articles_df = create_features(articles_df)
+
+    encoder = pickle.load(open('cat_cols_encoder_test.pkl', 'rb'))
+    articles_df[CAT_COLS] = encoder.transform(articles_df[CAT_COLS])
+
+    assert articles_df[CAT_COLS].any().any()
 
 
 def test_model_generates_predictions():
